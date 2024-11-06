11b0342f1e267cf103d913ecb717944407d49500
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-11-06 09:12:24 +0100

    in progress feature / adding Jump() and Move() Players Functions / TODO: Fix infinity jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed;
-    public float JumpForce;
+    public float speed = 4.0f;
+    public float jumpForce = 1.5f;
     private Rigidbody2D Rigidbody2D;
-    private float Horizontal;
+    private bool grounded;
+    private float horizontal;
+
+    private Vector2 input;
+    private Vector3 direction;
 
     private void Start()
     {
@@ -17,16 +21,34 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         // Movimiento
-        Horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        if (horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        // Detectar Suelo
+        if (Physics2D.Raycast(transform.position, Vector3.down, speed))
+        {
+            grounded = true;
+        }
+        else grounded = false;
+
+        // Salto
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
 
     }
 
     private void FixedUpdate()
     {
-        Rigidbody2D.linearVelocity = new Vector2(Horizontal, Rigidbody2D.linearVelocity.y);
+        Rigidbody2D.linearVelocity = new Vector2(horizontal * speed, Rigidbody2D.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * jumpForce);
     }
 
 }
