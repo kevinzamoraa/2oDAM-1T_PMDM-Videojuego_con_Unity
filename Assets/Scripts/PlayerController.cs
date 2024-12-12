5bf8c98cc0f746bcc8bb7c29d2bf0e37a074ad8b
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-12-12 16:12:04 +0100

    in progress / trying to solve infinite jumping and dead zone creation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private bool active = true;
     public float speed = 4.0f;
     public float jumpForce = 1.5f;
+    public float jumpCooldown = 0.2f;
+
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
+    private Collider2D collider;
+
     private bool grounded;
     private float horizontal;
+    private bool isJumping;
 
     private Vector2 input;
     private Vector3 direction;
-    private Animator Animator;
 
     private void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
         Animator = GetComponent<Animator>();
+        collider = GetComponent<Collider2D>();
     }
 
     private void Update()
@@ -25,21 +32,26 @@ public class PlayerController : MonoBehaviour
         // Movimiento
         horizontal = Input.GetAxisRaw("Horizontal");
         Animator.SetBool("running", horizontal != 0.0f);
+        Animator.SetBool("jumping", transform.position.y != 0.0f);
 
         if (horizontal < 0.0f) transform.localScale = new Vector3(-2.0f, 2.0f, 2.0f);
         else if (horizontal > 0.0f) transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
 
         // Detectar Suelo
-        if (Physics2D.Raycast(transform.position, Vector3.down, speed))
-        {
-            grounded = true;
-        }
-        else grounded = false;
+        grounded = Physics2D.Raycast(transform.position, Vector3.down, speed);
+        if (transform.position.y <= 0.0f) { isJumping = false; };
 
         // Salto
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        if (Input.GetKey(KeyCode.Space) && grounded && !isJumping)
         {
             Jump();
+            isJumping = true;
+        }
+
+        // Muerte & Reinicio del juego
+        if (!active)
+        {
+            return;
         }
 
     }
@@ -51,7 +63,27 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        Rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+        Rigidbody2D.AddForce(new Vector2(Rigidbody2D.linearVelocity.x, jumpForce));
+    }
+
+    private void MiniJump()
+    {
+        Rigidbody2D.linearVelocity = new Vector2(Rigidbody2D.linearVelocity.x, jumpForce / 2);
+    }
+
+    private void Die()
+    {
+        active = false;
+        collider.enabled = false;
+        MiniJump();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
     }
 
 }
