f84d2311ef98e8f8549f1b64fad5518ea0cd2b46
Kevin Zamora <kevin.zamora.amela@gmail.com>
2025-01-12 00:12:47 +0100

    in progress feature / Adding Enemies Movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,6 +3,36 @@ using UnityEngine.SceneManagement;
 
 public class EnemyController : MonoBehaviour
 {
+    public Transform player;
+    public float detectionRadius = 5.0f;
+    public float speed = 2.0f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionRadius)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            movement = new Vector2(direction.x, 0);
+        }
+        else
+        {
+            movement = Vector2.zero;
+        }
+
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+
     public int Respawn;
     void OnTriggerEnter2D(Collider2D other) {
         // Destroy(other.gameObject);
